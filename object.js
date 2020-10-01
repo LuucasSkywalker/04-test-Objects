cc6cc58e5b0fb76a9222eefcb9e50232a26062e3
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-09-30 23:05:16 -0300

    Estudo Objetos ..

---
+++ object.js
@@ -0,0 +1,51 @@
+const product = {}  //object empty
+product.name = 'Monitor', 
+product.value = '2100,00',
+
+console.log(product);
+
+//array separado por virgula .
+
+const name  = {}
+name.pessoa1 = 'Lucas',
+name.pessoa2 = 'Larissa';
+console.log(name);
+
+//IMPORTANTE, usa sempre VIRGULA dentro de objetos
+
+const smartphone = {
+
+      
+      nameproduct1: 'Iphone',
+      value: ' 4500.00',
+      releasedateof: 'November',
+      
+        
+      nameproduct2: 'samsung',
+      value2: '3900.00',
+      releasedaof: 'October',
+       
+      releasedateiphone: {
+
+         starting: 17,
+
+          releasedatesamsung: {
+       
+            starting: 25,
+
+          }
+     }  
+    
+}
+
+console.log(smartphone)
+
+const person = {
+
+  name: 'Lucas',
+  age: 29,
+  city: 'Caldas Novas'
+};
+const name1 = person.name
+const age1 = person.age
+console.log(name1,age1)
