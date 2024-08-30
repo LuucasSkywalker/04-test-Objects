e9eda5b67d34bc007449cf86ebc685ee59c31dd7
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2024-08-30 20:07:45 -0300

    Att

--- object.js
+++ object.js
@@ -1,3 +1,52 @@
+
+/*
+Um par de chaves {} representa um objeto,
+o que e um objeto 'UM CONJUNTO DE CHAVES,PARES E VALORES' 
+que tem pares EXEMPLO ( (nome: 'blusa') o nome do produto e o valor que e a blusa) .
+podendo conter varios objetos dentro de si proprio  */
+
+let produt = {}
+produt.blusa = "cor preta";
+produt.preco = 56.50
+console.log(produt) /* de forma literal adicionando valores
+fora das chaves do object usa o ( . ) para adicionar valor*/
+
+const car = {
+    fabricar: 'carro',
+     adicionar: 'chassi',
+      adicionar2: 'lataria',
+       adiconar3: 'motor',
+        adicionar4: 'eletrica',
+         adiconar5: 'rodas',
+
+};
+
+console.log(car.adicionar2)
+
+
+const papelaria = {   //adicionando valores do objeto dentro do objeto usando ( : )
+    papeis:['chamex', 'sufiti','papel cartao','papel mateiga','papel ceda'], //array dentro de um objeto
+    papeisembrulho: ['cartao', 'papel de presente, '],
+    tiposDeCaneta: { //adicionando outro objeto dentro de um ja existente 
+        caneta: 'preta',
+        caneta2: 'azul',
+        caneta3: 'verde',
+        caneta4: 'vermelha',
+                   }
+        
+};
+console.log(papelaria.papeisembrulho[1]) // selecionando valor dentro do array no objeto
+console.log(papelaria.tiposDeCaneta.caneta4)
+
+class pessoa{}
+console.log(typeof pessoa)
+const pessoa100 = new pessoa()
+
+console.log(typeof pessoa100)
+//console.log(papelaria.papeis[2])
+//console.log(papelaria.tiposDeCaneta.caneta4)
+
+
 const product = {}  //object empty
 product.name = 'Monitor', 
 product.value = '2100,00',
@@ -42,14 +91,14 @@ console.log(smartphone);
 
 /************************************************** */
 
-const person = {
+let pessoa = {
 
   name: 'Lucas',
   age: 29,
   city: 'Caldas Novas'
 };
-const name1 = person.name
-const age1 = person.age
+const name1 = pessoa.name
+const age1 = pessoa.age
 console.log(name1,age1);
 
 /**************************************************** */
@@ -61,7 +110,7 @@ const person = {
       return this.hello  //referênciando THIS a person
   }
 };
-console.log(person.speak());    //retorna "good morning"
+console.log(person1.speak());    //retorna "good morning"
 
 //ou usar assim
 const person1 = {
